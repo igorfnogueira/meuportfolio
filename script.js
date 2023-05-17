601fad6f35fab8a4ded91f58f6b65e6c216b4ecf
Igor Fonseca Nogueira <38932268+igorfnogueira@users.noreply.github.com>
2023-05-17 19:43:08 -0300

    update footer

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var imgAtual = '/img/igor.png'
 var imgAnterior = '/img/igor2.png'
 
-
+var corBackgroudAnterior='#9BA4B5'
+var corBackgroundAtual='#F1F6F9'
 
 
 function trocarFoto() {
@@ -9,5 +10,12 @@ function trocarFoto() {
   let aux = imgAtual
   imgAtual = imgAnterior
   imgAnterior = aux
+}
+
+function trocarCor() {
+  document.getElementByquerySelector("body").style.color=corBackgroundAtual
+  let aux = corBackgroundAtual
+  corBackgroundAtual = corBackgroundAnterior
+  corBackgroundAnterior = aux
 }
 
