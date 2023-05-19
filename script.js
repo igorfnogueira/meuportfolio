45e7bc8893ceb0271a7d99becb3de3d235f0bb46
Igor Fonseca Nogueira <38932268+igorfnogueira@users.noreply.github.com>
2023-05-19 16:48:32 -0300

    dark mode added

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 var imgAtual = '/img/igor.png'
 var imgAnterior = '/img/igor2.png'
 
-var corBackgroudAnterior='#9BA4B5'
-var corBackgroundAtual='#F1F6F9'
+
+
+var corBackgroundAnterior='#F1F6F9'
+var corBackgroundAtual='#212A3E'
+var corTextoAnterior='black'
+var corTextoAtual='white'
+var corHeaderAnterior='#212A3E'
+var corHeaderAtual='#0f131d'
 
 
 function trocarFoto() {
@@ -13,9 +19,31 @@ function trocarFoto() {
 }
 
 function trocarCor() {
-  document.getElementByquerySelector("body").style.color=corBackgroundAtual
+//  cor body
+  document.querySelector("body").style.backgroundColor=corBackgroundAtual
   let aux = corBackgroundAtual
   corBackgroundAtual = corBackgroundAnterior
   corBackgroundAnterior = aux
+
+ 
+  
+  // cor texto
+  document.querySelector("#projetos").style.color=corTextoAtual
+  document.querySelector("#bio").style.color=corTextoAtual
+  // document.querySelector("a").style.color=corTextoAtual
+  let aux2 = corTextoAtual
+  corTextoAtual = corTextoAnterior
+  corTextoAnterior = aux2
+
+   //cor header e footer
+   document.querySelector("header").style.backgroundColor=corHeaderAtual
+   document.querySelector("footer").style.backgroundColor=corHeaderAtual
+   let aux3 = corHeaderAtual
+   corHeaderAtual = corHeaderAnterior
+   corHeaderAnterior = aux3
+
+
+
 }
+
 
